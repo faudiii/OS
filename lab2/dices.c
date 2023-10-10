dc2c5487533b1218f92b538e0d4afdd54d2713f8
alex n <alexnaf@womanup.online>
2023-10-10 22:43:15 +0300

    fix: вроде работает

--- lab2/dices.c
+++ lab2/dices.c
@@ -1,36 +1,64 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 #include <time.h>
 
 int player1_wins = 0;
 int player2_wins = 0;
 int draw = 0;
-int player1_total, player2_total;
+int player1_total;
+int player2_total;
+
 pthread_mutex_t mutex;
 
-void calculate_wins(int i, int j) {
-    if (player1_total + i > player2_total + j) {
-        player1_wins++;
-    } else if (player1_total + i < player2_total + j) {
-        player2_wins++;
-    } else {
-        draw++;
+struct ThreadArgs
+{
+    int K;
+    int player1_total;
+    int player2_total;
+};
+
+void *run_experiment(void *args)
+{
+    struct ThreadArgs *threadArgs = (struct ThreadArgs *)args;
+    int K = threadArgs->K;
+    int player1_score = 0;
+    int player2_score = 0;
+
+    for (int i = 0; i < K; i++)
+    {
+        player1_score += (rand() % 6 + 1) + (rand() % 6 + 1);
+        player2_score += (rand() % 6 + 1) + (rand() % 6 + 1);
     }
+
+    int player1_total_with_score = threadArgs->player1_total + player1_score;
+    int player2_total_with_score = threadArgs->player2_total + player2_score;
+
+    if (player1_total_with_score > player2_total_with_score)
+    {
+        pthread_mutex_lock(&mutex);
+        player1_wins++;
+        pthread_mutex_unlock(&mutex);
+    }
+    else if (player1_total_with_score < player2_total_with_score)
+    {
+        pthread_mutex_lock(&mutex);
+        player2_wins++;
+        pthread_mutex_unlock(&mutex);
+    }
+    else
+    {
+        pthread_mutex_lock(&mutex);
+        draw++;
+        pthread_mutex_unlock(&mutex);
+    }
+
+    pthread_exit(NULL);
 }
 
-// void* calculate_wins_thread(void* args) {
-//     int* thread_args = (int*)args;
-//     int i = thread_args[0];
-//     int j = thread_args[1];
-    
-//     calculate_wins(i, j);
-    
-//     pthread_exit(NULL);
-// }
-
-int main() {
-    int K;
-    int max_threads;
+int main()
+{
+    int K, experiments, max_threads;
 
     printf("Enter the number of rounds (K): ");
     scanf("%d", &K);
@@ -41,48 +69,45 @@ int main() {
     printf("Enter player 2's total score: ");
     scanf("%d", &player2_total);
 
+    printf("Enter the number of experiments: ");
+    scanf("%d", &experiments);
+
     printf("Enter the maximum number of threads: ");
     scanf("%d", &max_threads);
-
-    int min_points = K * 2;
-    int max_points = K * 12;
 
     pthread_mutex_init(&mutex, NULL);
 
     pthread_t tid[max_threads];
-    
-    int thread_args[max_threads][2];
+    struct ThreadArgs threadArgs;
 
-    int count = 0;
+    threadArgs.K = K;
+    threadArgs.player1_total = player1_total;
+    threadArgs.player2_total = player2_total;
 
-    clock_t start_time = clock(); 
+    clock_t start_time = clock();
 
+    for (int i = 0; i < experiments; i++)
+    {
+        pthread_create(&tid[i % max_threads], NULL, run_experiment, &threadArgs);
 
-    for (int i = min_points; i <= max_points; i++) {
-        for (int j = min_points; j <= max_points; j++) {
-            thread_args[count][0] = i;
-            thread_args[count][1] = j;
-            pthread_create(&tid[count], NULL, calculate_wins, &thread_args[count]);
-            count++;
-
-            if (count == max_threads) {
-                for (int k = 0; k < max_threads; k++) {
-                    pthread_join(tid[k], NULL);
-                }
-                count = 0; // Сбрасываем счетчик потоков
+        if ((i + 1) % max_threads == 0)
+        {
+            for (int j = 0; j < max_threads; j++)
+            {
+                pthread_join(tid[j], NULL);
             }
         }
     }
 
-    clock_t end_time = clock(); 
-    double execution_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
-
-    // Ожидание оставшихся потоков
-    for (int k = 0; k < count; k++) {
-        pthread_join(tid[k], NULL);
+    for (int i = 0; i < experiments % max_threads; i++)
+    {
+        pthread_join(tid[i], NULL);
     }
 
     pthread_mutex_destroy(&mutex);
+
+    clock_t end_time = clock();
+    double execution_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
 
     printf("Player 1 wins: %d times\n", player1_wins);
     printf("Player 2 wins: %d times\n", player2_wins);
